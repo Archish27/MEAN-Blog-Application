da5ea1b53688eecc2b57415fb42e7df0e1369649
Archish Thakka <archishthakkar@gmail.com>
2018-04-12 23:54:26 +0530

    Login

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
 
 module.exports = (router) => {
     router.post('/register', (req, res) => {
@@ -36,39 +38,80 @@ module.exports = (router) => {
         // req.body.password;
     });
 
-    router.get('/checkEmail/:email',(req,res)=>{
-        if(!req.params.email){
-            res.json({success: false,message:'E-mail was not provided'});
-        }else{
-            User.findOne({email:req.params.email},(err,user)=>{
-                if(err){
-                    res.json({success:false,message:err});
-                }else{
-                    if(!user){
-                        res.json({success:false,message:'E-mail is already taken'});
-                    }else{
-                        res.json({success:true,message:'E-mail is available'});
+    router.get('/checkEmail/:email', (req, res) => {
+        if (!req.params.email) {
+            res.json({success: false, message: 'E-mail was not provided'});
+        } else {
+            User.findOne({email: req.params.email}, (err, user) => {
+                if (err) {
+                    res.json({success: false, message: err});
+                } else {
+                    if (!user) {
+                        res.json({success: false, message: 'E-mail is already taken'});
+                    } else {
+                        res.json({success: true, message: 'E-mail is available'});
                     }
                 }
             });
         }
     });
-    router.get('/checkUsername/:username',(req,res)=>{
-        if(!req.params.username){
-            res.json({success: false,message:'Username was not provided'});
-        }else{
-            User.findOne({username:req.params.username},(err,user)=>{
-                if(err){
-                    res.json({success:false,message:err});
-                }else{
-                    if(!user){
-                        res.json({success:false,message:'Username is already taken'});
-                    }else{
-                        res.json({success:true,message:'Username is available'});
+    router.get('/checkUsername/:username', (req, res) => {
+        if (!req.params.username) {
+            res.json({success: false, message: 'Username was not provided'});
+        } else {
+            User.findOne({username: req.params.username}, (err, user) => {
+                if (err) {
+                    res.json({success: false, message: err});
+                } else {
+                    if (!user) {
+                        res.json({success: false, message: 'Username is already taken'});
+                    } else {
+                        res.json({success: true, message: 'Username is available'});
                     }
                 }
             });
         }
     });
+
+    router.post('/login', (req, res) => {
+        if (!req.body.username) {
+            res.json({success: false, message: 'No Username was provided'});
+        } else {
+            if (!req.body.password) {
+                res.json({success: false, message: 'No Password was provided'});
+            } else {
+                User.findOne({username: req.body.username.toLowerCase()}, (err, user) => {
+                    if (err) {
+                        res.json({success: false, message: err});
+                    }
+                    else {
+                        if (!user) {
+                            res.json({success: false, message: err});
+                        } else {
+                            const validPassword = user.comparePassword(req.body.password);
+                            if (!validPassword) {
+                                res.json({success: false, message: 'Password Invalid'});
+                            } else {
+
+                                const token = jwt.sign({
+                                    userId: user._id
+                                }, config.secret, {expiresIn: '24h'});
+
+                                res.json({
+                                    success: true,
+                                    message: 'Success!',
+                                    token: token,
+                                    user: {username: user.username}
+                                })
+                            }
+
+                        }
+                    }
+                });
+
+            }
+        }
+    });
+
     return router;
 };
